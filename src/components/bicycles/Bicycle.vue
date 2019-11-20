e85ed7d16e0bac55fca7ad8526cc9258f74b05bb
Alex <electrikalex@gmail.com>
2019-11-20 18:15:33 +0000

    Add delete picture

--- src/components/bicycles/Bicycle.vue
+++ src/components/bicycles/Bicycle.vue
@@ -49,6 +49,7 @@
 
 <script>
 import { db } from "@/firebase";
+import { storage } from "@/firebase";
 export default {
   data() {
     return {
@@ -73,6 +74,10 @@ export default {
         db.ref("people/" + this.bicycle.currentUser + "/bicycleID").set(null);
       }
       db.ref("bicycles/" + key).remove();
+      storage
+        .ref()
+        .child("bicycle" + this.bicycle.id)
+        .delete();
     }
   },
   computed: {
