437598893d92021f046b1fa7abe0c1ae4c9d3015
Alex <electrikalex@gmail.com>
2019-12-03 15:37:54 +0000

    Capitalise orgs

--- src/components/users/UserTable.vue
+++ src/components/users/UserTable.vue
@@ -151,6 +151,8 @@ export default {
         {
           key: "organisation",
           label: "Organisation",
+          class: "text-capitalize text-left",
+
           sortable: true
         },
         {
