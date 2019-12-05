50439ca268afc5779c4340d4ef403c818bcf358a
Alex <electrikalex@gmail.com>
2019-12-05 20:09:29 +0000

    Added dummy account

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,21 @@
     <router-view />
   </div>
 </template>
+<script>
+import { auth } from "@/firebase";
+
+export default {
+  created: function() {
+    auth.onAuthStateChanged(function(user) {
+      if (user.uid != "dmfqlj7qiZcSuznZiiPRt2Ja2yi2") {
+        alert(
+          "You have logged in with a viewing account, which means that you will not be able to add, edit, or delete any of the users and bicycles. You can click any button you want without affecting the data."
+        );
+      }
+    });
+  }
+};
+</script>
 <style media="screen">
 @media (min-width: 576px) {
   .container {
